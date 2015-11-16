4197e7a364d227e01a3a69dfdf69d54dc67350fe
Tprezioso <tommyprezioso@gmail.com>
2015-11-16 14:44:54 -0500

    refactoting some stuff

--- CleanUrLungs/ViewController.m
+++ CleanUrLungs/ViewController.m
@@ -24,12 +24,6 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 - (IBAction)cravingButton:(id)sender
 {
 
