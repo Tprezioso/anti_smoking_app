a0c805e49242071c548e596eb3a6d53171fe46e5
Tprezioso <tommyprezioso@gmail.com>
2015-12-22 12:31:38 -0500

    still trying to work on this damn notification

--- CleanUrLungs/ViewController.m
+++ CleanUrLungs/ViewController.m
@@ -41,6 +41,7 @@
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(resetLabels:) name:@"changedDayNotification" object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(timeChanged:) name:UIApplicationSignificantTimeChangeNotification object:nil];
     [self weekLaterReduceDailyCig];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationSignificantTimeChangeNotification object:nil];
 }
 
 - (void)timeChanged:(NSNotification *)notification
