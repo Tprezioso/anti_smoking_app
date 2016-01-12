84a67918feacfae67cb890c14259d77deccafb3d
Tprezioso <tommyprezioso@gmail.com>
2016-01-12 13:20:02 -0500

    still working on time change problem

--- CleanUrLungs/AppDelegate.m
+++ CleanUrLungs/AppDelegate.m
@@ -21,7 +21,9 @@
     // Override point for customization after application launch.
     UIUserNotificationSettings* notificationSettings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert | UIUserNotificationTypeBadge | UIUserNotificationTypeSound categories:nil];
     [[UIApplication sharedApplication] registerUserNotificationSettings:notificationSettings];
-     
+    [[NSNotificationCenter defaultCenter] postNotificationName:UIApplicationSignificantTimeChangeNotification object:nil userInfo:nil];
+    [self applicationSignificantTimeChange:[UIApplication sharedApplication]];
+  
     return YES;
 }
 
@@ -29,6 +31,9 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+//    UIBackgroundTaskIdentifier identifier = [application beginBackgroundTaskWithExpirationHandler:nil];
+//    [[NSNotificationCenter defaultCenter] postNotificationName:UIApplicationSignificantTimeChangeNotification object:nil userInfo:nil];
+//    [application endBackgroundTask:identifier];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
@@ -45,6 +50,8 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+  //  [[NSNotificationCenter defaultCenter] postNotificationName:UIApplicationSignificantTimeChangeNotification object:nil userInfo:nil];
+
 }
 
 - (void)applicationSignificantTimeChange:(UIApplication *)application
