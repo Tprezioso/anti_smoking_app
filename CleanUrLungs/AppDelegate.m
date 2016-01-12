758570bd0d114a5724f26dfbb0ddd398f12c2b29
Tprezioso <tommyprezioso@gmail.com>
2016-01-12 13:47:08 -0500

    working on background checking

--- CleanUrLungs/AppDelegate.m
+++ CleanUrLungs/AppDelegate.m
@@ -22,7 +22,6 @@
     UIUserNotificationSettings* notificationSettings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert | UIUserNotificationTypeBadge | UIUserNotificationTypeSound categories:nil];
     [[UIApplication sharedApplication] registerUserNotificationSettings:notificationSettings];
     [[NSNotificationCenter defaultCenter] postNotificationName:UIApplicationSignificantTimeChangeNotification object:nil userInfo:nil];
-    [self applicationSignificantTimeChange:[UIApplication sharedApplication]];
   
     return YES;
 }
@@ -31,15 +30,17 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-//    UIBackgroundTaskIdentifier identifier = [application beginBackgroundTaskWithExpirationHandler:nil];
-//    [[NSNotificationCenter defaultCenter] postNotificationName:UIApplicationSignificantTimeChangeNotification object:nil userInfo:nil];
-//    [application endBackgroundTask:identifier];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+//    UIBackgroundTaskIdentifier identifier = [application beginBackgroundTaskWithExpirationHandler:^{
+//        [[NSNotificationCenter defaultCenter] postNotificationName:UIApplicationSignificantTimeChangeNotification object:nil userInfo:nil];
+//        
+//    }];
+//    [application endBackgroundTask:identifier];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -50,8 +51,6 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-  //  [[NSNotificationCenter defaultCenter] postNotificationName:UIApplicationSignificantTimeChangeNotification object:nil userInfo:nil];
-
 }
 
 - (void)applicationSignificantTimeChange:(UIApplication *)application
