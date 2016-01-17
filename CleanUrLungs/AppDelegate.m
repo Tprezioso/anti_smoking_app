cd33805dd5b7a7d9aff139598d34868908c10027
Tprezioso <tommyprezioso@gmail.com>
2016-01-16 19:38:19 -0500

    refactoring

--- CleanUrLungs/AppDelegate.m
+++ CleanUrLungs/AppDelegate.m
@@ -35,11 +35,6 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-//    UIBackgroundTaskIdentifier identifier = [application beginBackgroundTaskWithExpirationHandler:^{
-//        [[NSNotificationCenter defaultCenter] postNotificationName:UIApplicationSignificantTimeChangeNotification object:nil userInfo:nil];
-//        
-//    }];
-//    [application endBackgroundTask:identifier];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -51,11 +46,6 @@
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
-
-//- (void)applicationSignificantTimeChange:(UIApplication *)application
-//{
-//    [[NSNotificationCenter defaultCenter] postNotificationName:UIApplicationSignificantTimeChangeNotification object:nil userInfo:nil];
-//}
 
 - (BOOL)application:(UIApplication *)application willFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
