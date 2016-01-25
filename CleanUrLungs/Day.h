88a6e9b7236eaea504bf808e9cec26cf130f5018
Tprezioso <tommyprezioso@gmail.com>
2016-01-25 15:38:44 -0500

    added Day class

---
+++ CleanUrLungs/Day.h
@@ -0,0 +1,13 @@
+//
+//  Day.h
+//  CleanUrLungs
+//
+//  Created by Thomas Prezioso on 1/25/16.
+//  Copyright © 2016 Thomas Prezioso. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Day : NSObject
+
+@end
---
+++ CleanUrLungs/Day.m
@@ -0,0 +1,13 @@
+//
+//  Day.m
+//  CleanUrLungs
+//
+//  Created by Thomas Prezioso on 1/25/16.
+//  Copyright © 2016 Thomas Prezioso. All rights reserved.
+//
+
+#import "Day.h"
+
+@implementation Day
+
+@end
