dc265fb1b8339d7d811ef7bc29201e0419da65ce
Tprezioso <tommyprezioso@gmail.com>
2016-02-07 11:39:48 -0500

    refactoring

--- CleanUrLungs/Day.m
+++ CleanUrLungs/Day.m
@@ -33,6 +33,7 @@
     NSManagedObjectContext *context = [self managedObjectContext];
     NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] initWithEntityName:@"Date"];
     days = [[context executeFetchRequest:fetchRequest error:nil] mutableCopy];
+
     for (NSInteger i = 0; i < [days count]; i++) {
         Day *savedDay = [Day new];
         savedDay.date = [days[i] valueForKey:@"dateSaved"];
