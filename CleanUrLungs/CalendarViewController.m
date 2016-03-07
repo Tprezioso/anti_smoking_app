35f2c0a3744538273d532d05165be5773ce65009
Tprezioso <tommyprezioso@gmail.com>
2016-03-07 17:35:37 -0500

    uncommented prepareForSegue in calendarVC

--- CleanUrLungs/CalendarViewController.m
+++ CleanUrLungs/CalendarViewController.m
@@ -68,12 +68,11 @@
     return [[NSCalendar currentCalendar] dateFromComponents:comps];
 }
 
-/*
 #pragma mark - Navigation
 // In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
 @end
