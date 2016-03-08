a8bbf15d0248c6e93016b40a9eb742362f1f061b
Tprezioso <tommyprezioso@gmail.com>
2016-03-08 16:38:15 -0500

    added detailCalendarVC

---
+++ CleanUrLungs/DetailCalendarViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailCalendarViewController.h
+//  CleanUrLungs
+//
+//  Created by Thomas Prezioso on 3/8/16.
+//  Copyright © 2016 Thomas Prezioso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailCalendarViewController : UIViewController
+
+@end
---
+++ CleanUrLungs/DetailCalendarViewController.m
@@ -0,0 +1,33 @@
+//
+//  DetailCalendarViewController.m
+//  CleanUrLungs
+//
+//  Created by Thomas Prezioso on 3/8/16.
+//  Copyright © 2016 Thomas Prezioso. All rights reserved.
+//
+
+#import "DetailCalendarViewController.h"
+#import "Day.h"
+
+@interface DetailCalendarViewController ()
+
+@end
+
+@implementation DetailCalendarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
