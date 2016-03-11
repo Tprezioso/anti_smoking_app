9b1a8b5c57b1755fdf3d5f42a3795ef6f2f167d8
Tprezioso <tommyprezioso@gmail.com>
2016-03-11 15:41:46 -0500

    refactoring out more code

--- CleanUrLungs/ViewController.m
+++ CleanUrLungs/ViewController.m
@@ -78,16 +78,7 @@
 
 - (void)timeChanged
 {
-//    NSString *savedCigSmokedValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"cigSmokedValue"];
-//    savedCigSmokedValue = @"0";
-//    [[NSUserDefaults standardUserDefaults] setObject:savedCigSmokedValue forKey:@"cigSmokedValue"];
-//    [[NSUserDefaults standardUserDefaults] synchronize];
     self.smokedLabel.text = @"0";
-
-//    NSString *savedCravedValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"cravedSaved"];
-//    savedCravedValue = @"0";
-//    [[NSUserDefaults standardUserDefaults] setObject:savedCravedValue forKey:@"cravedSaved"];
-//    [[NSUserDefaults standardUserDefaults] synchronize];
     self.cravingLabel.text = @"0";
 }
 
