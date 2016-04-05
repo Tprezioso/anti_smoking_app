9f30c8ef850093d2385ada6ba7dd90bef6262285
Tprezioso <tommyprezioso@gmail.com>
2016-04-05 16:35:25 -0400

    added setting VC and did some constraints

---
+++ CleanUrLungs/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  
+//
+//  Created by Thomas Prezioso on 4/5/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
---
+++ CleanUrLungs/SettingsViewController.m
@@ -0,0 +1,39 @@
+//
+//  SettingsViewController.m
+//  
+//
+//  Created by Thomas Prezioso on 4/5/16.
+//
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+@property (strong, nonatomic) IBOutlet UIButton *mondayButton;
+@property (strong, nonatomic) IBOutlet UIButton *clearAllData;
+
+@end
+
+@implementation SettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
