9cb42d92a3dd5771909c73222d5d0713775c6519
Tprezioso <tommyprezioso@gmail.com>
2016-04-11 15:08:35 -0400

    added commented segue code in case I make a notesVC

--- CleanUrLungs/CalendarViewController.m
+++ CleanUrLungs/CalendarViewController.m
@@ -187,5 +187,14 @@
     NSString *dateString = [dateFormat stringFromDate:self.dateToPass];
     return dateString;
 }
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
