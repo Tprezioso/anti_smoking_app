e8ad721d268994fe40bd366d492eb589d181eb1b
Tprezioso <tommyprezioso@gmail.com>
2016-08-02 12:40:29 -0400

    changes notification to ever 4 hours

--- CleanUrLungs/AppDelegate.m
+++ CleanUrLungs/AppDelegate.m
@@ -73,7 +73,7 @@
     NSString *smokedString = @"";
     smokedString = [[NSUserDefaults standardUserDefaults] objectForKey:@"cigSmokedValue"];
     UILocalNotification *notification = [[UILocalNotification alloc] init];
-    notification.fireDate = [[NSDate date] dateByAddingTimeInterval:60 * 60 * 24];
+    notification.fireDate = [[NSDate date] dateByAddingTimeInterval:60 * 60 * 4];
     notification.alertBody = [NSString stringWithFormat: @"You Have Smoked %@ So Far Today",smokedString];
     [[UIApplication sharedApplication] scheduleLocalNotification:notification];
 }
